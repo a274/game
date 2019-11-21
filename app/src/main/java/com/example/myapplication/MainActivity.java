cf8f177fe3f623d4f4fc348462e17030926a18f9
NetLight <jasoness@yandex.ru>
2019-11-21 18:53:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView debugText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        debugText=(TextView)findViewById(R.id.debugText);
+        Unit alexey = new Unit("Alexey Grigorevich", 200);
+        alexey.printInfo(debugText);
+        alexey.letsGO(debugText);
+        Robot Shwarzenegger = new Robot("Arnold Shwarzenegger", 1000, 400);
+        Shwarzenegger.printInfo(debugText);
+        Shwarzenegger.letsGO(debugText);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Unit.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.widget.TextView;
+
+public class Unit {
+    public String getName() {
+        return name;
+    }
+
+    private String name = "Nill";
+    private int health =100;
+
+    public Unit(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public void printInfo(TextView textPlace) {
+        textPlace.append("Меня зовут " + name + ", и я имею " + health + " hp. \n");
+    }
+    public void letsGO(TextView textPlace) {
+        textPlace.append(name + " начал бежать. \n");
+    }
+}
